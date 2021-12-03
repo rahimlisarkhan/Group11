9c984a16ccd2b5f26896be6a1c82dbd21eb420c1
rahimlisarkhan <rahimlisarkhan@gmail.com>
2021-12-03 22:39:52 +0400

    update

---
+++ topic10/calcFunc/index.js
@@ -0,0 +1,60 @@
+//Dom elementleri
+let birinciReqem = document.querySelector('#birinciReqem')
+let ikinciReqem = document.querySelector('#ikinciReqem')
+let operatorAdi = document.querySelector('#operatorAdi')
+let netice = document.querySelector('#netice')
+
+
+//Tutdugumuz hesablama ucun olan deyerler
+let firstNum = ''
+let secondNum = ''
+let operator = null
+let siralama = false
+let neticeniGor = ''
+
+
+
+function emeliyyat(e) {
+    let duymeninDeyeri = e.value
+
+    
+    //Birinci Reqem ucun yazilan
+    if (duymeninDeyeri !== "+" && duymeninDeyeri !== "*" && duymeninDeyeri !== "beraberdir" && siralama === false) {
+        firstNum +=  duymeninDeyeri
+    } else if (duymeninDeyeri !== "+" && duymeninDeyeri !== "*" && duymeninDeyeri !== "beraberdir" && siralama === true) {
+        secondNum += duymeninDeyeri
+    }
+
+    //Operator ucun yazilan Reqem ucun yazilan
+    if (duymeninDeyeri === "+" || duymeninDeyeri === "*") {
+        operator = duymeninDeyeri
+        siralama = true
+    }
+
+    //Artiq Beraberdir isleyende
+    if (duymeninDeyeri === "beraberdir") {
+       
+        if(operator === "*"){
+            console.log('Vurma isi gorulur');
+            neticeniGor = parseInt(firstNum) * parseInt(secondNum)
+
+        }else if(operator === "+"){
+            console.log('Toplama isi gorulur');
+            neticeniGor = parseInt(firstNum) + parseInt(secondNum)
+        }
+
+    }
+
+    //DOMa yazirma hissesi
+    birinciReqem.innerHTML = firstNum
+    operatorAdi.innerHTML = operator
+    ikinciReqem.innerHTML = secondNum
+    netice.innerHTML =`Result ${neticeniGor} `
+
+    // console.log('-------------------');
+    // console.log('birinci', firstNum);
+    // console.log('Ope', operator);
+    // console.log('ikinci', secondNum);
+    // console.log(neticeniGor);
+
+}
---
+++ topic10/index.js
@@ -0,0 +1,89 @@
+// var a = 12
+// var b = 12
+
+// istifade(a)
+// istifade(b)
+
+
+
+
+// function artirir(num){
+//    return ++num
+
+// }
+// function logdaGoster(eded){
+//     console.log(eded);
+// }
+
+// function istifade(gelen){
+//     let netice = artirir(gelen)
+//     logdaGoster(netice)
+// }
+
+
+
+// class Personal{
+//     fullname ="Elsad "
+
+//     // constructor(ad) {
+//     //     this.fullname = ad
+//     // }
+
+// }
+
+// class dovlet extends Personal{
+//     vezife =" Developer"
+    
+    
+//     // fullname ="Elsad "
+
+//     // constructor(vezifesi){
+//     //     super()
+
+//     //     this.vezife = vezifesi
+//     // }
+
+
+// }
+
+
+// class Qesebe extends dovlet{
+
+//     constructor(yeniV,yeniAd){
+//         super()
+//         this.fullname = yeniAd
+//         this.vezife = yeniV
+//     }
+    
+
+//     goster(){
+//         console.log(this.fullname,this.vezife);
+//     }
+
+// }
+
+// let qesebe1 = new Qesebe('Lead Developer')
+
+// qesebe1.goster()
+// console.log(qesebe1);
+// let person1 = new dovlet("Lead")
+
+// console.log(person1);
+// console.log(person2);
+// console.log(person3);
+// // let obj1 ={
+//     fullname:"Xeyyam"
+// }
+
+// let obj2 ={
+//     fullname:"Murad"
+// }
+
+// let obj3 ={
+//     fullname:"Sabina"
+// }
+
+
+
+
+
