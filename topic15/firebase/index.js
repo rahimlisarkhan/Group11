82e78efc3ff7863d46663aad9bb227a5735b5561
rahimlisarkhan <rahimlisarkhan@gmail.com>
2021-12-16 01:43:06 +0400

    update

--- topic15/firebase/index.js
+++ topic15/firebase/index.js
@@ -1,19 +1,140 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBRszLosmFHNfv0H4qzpxVjMYzmgd5XGqk",
+    authDomain: "todoapp-512ca.firebaseapp.com",
+    databaseURL: "https://todoapp-512ca-default-rtdb.firebaseio.com",
+    projectId: "todoapp-512ca",
+    storageBucket: "todoapp-512ca.appspot.com",
+    messagingSenderId: "74998074232",
+    appId: "1:74998074232:web:9c1187615194b10ccd168d"
+};
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBqXbMmpyajfrl8SGH6NQ9S-4hu-mbun1k",
-//   authDomain: "loginsystem-9fc9c.firebaseapp.com",
-//   databaseURL: "https://loginsystem-9fc9c-default-rtdb.firebaseio.com",
-//   projectId: "loginsystem-9fc9c",
-//   storageBucket: "loginsystem-9fc9c.appspot.com",
-//   messagingSenderId: "357638075980",
-//   appId: "1:357638075980:web:2be0bed464444aa329e6e9"
-// };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
 
-// // Initialize Firebase
-// firebase.initializeApp(firebaseConfig)
-// let db = firebase.database();
+let db = firebase.database();
 
-// let data = {title:"Paolo"}
-// db.ref('about').set(data)
-// db.ref('books').push().set(data)
+// let count = 0
 
+
+//Database yazirma WRITE --------------------------------------------
+// document.querySelector('#go').onclick = function () {
+//     count++
+
+//     db.ref("/jobs").set({
+//         count: count,
+//         price: 11,
+//         total_price: 11 * count
+//     })
+
+// }
+
+// let usersBudagi = db.ref("/users/")
+
+// document.querySelector('#go').onclick = function () {
+//     count++
+
+// usersBudagi.push().set({
+//     full_name: "Yasar",
+//     age: 22
+// })
+
+// }
+
+
+//Update ucun ---------------------------------------------------------
+// document.querySelector('#go').onclick = function () {
+//     count++
+
+//     db.ref("/users/-MqynhoZSDRCSQuzpmln").set({
+//         full_name: "Elsad", // evvel john idi
+//         age: 24
+//     })
+
+// }
+
+// var globalData = null
+
+//Read-----------------------------------------------------
+// usersBudagi.on("value", function (snap) {
+//     let data = snap.val()
+
+//     let yeniArrayIDli = Object.entries(data).map(kicikArrayler => {
+//         istifadeciObyekti = {
+//             id: kicikArrayler[0],
+//             ...kicikArrayler[1]
+//         }
+
+//         return istifadeciObyekti
+//     })
+
+//     globalData = yeniArrayIDli
+
+//     render(globalData)
+// })
+
+
+// setTimeout(function(){
+//     console.log(globalData);
+// },5000)
+
+
+// let jobsBudagi = db.ref('jobs')
+
+// jobsBudagi.on("value",function(snap){
+
+
+//     if(snap.child('price').exists()){
+//         snap.child('price').set(115)
+
+
+//     }else{
+//         console.log('Get admini yarat');
+//     }
+
+//     console.log(deyer);
+// })
+
+
+// jobsBudagi.child('count').set(105);
+
+
+
+// let usersBudagi = db.ref('users')
+
+// usersBudagi.on("value",function( snap ){
+//     let saylari = snap
+
+//     console.log( saylari.numChildren() );
+
+// })
+
+
+
+
+
+
+let userAdi = prompt('Adiniz nedi?')
+
+let connectionsUser = db.ref('/connections')
+
+
+connectionsUser.once("value", function () {
+
+    let userGeldi = connectionsUser.push({
+        fullName: userAdi
+    })
+
+    userGeldi.onDisconnect().remove()
+
+})
+
+
+
+
+connectionsUser.on("value", function (snap) {
+    let div = document.createElement('h1')
+    div.innerText = snap.numChildren()
+
+    document.body.appendChild(div)
+})
---
+++ topic15/momentjs/index.js
@@ -0,0 +1,48 @@
+let randomdate = "17/02/1995"
+let randomFormat = "DD/MM/YYYY"
+
+//Format Momment
+// let time = moment().format(randomdate,randomFormat)
+// let time = moment("17-02-1995", "DD-MM-YYYY").fromNow()
+// let time = moment(randomdate,randomFormat) //wrong
+
+
+
+//Diffrent
+// let nowTime = moment().format(randomFormat)
+
+// let diffYear = moment().diff(moment().format(randomFormat),"years")
+
+
+// console.log(diffYear);
+
+
+var indikiTarix = moment();
+var kecmisTarix = moment([2018.02]);
+
+
+// // a.diff(b, 'years'); // 1
+
+// let illerArasindakiFerq = indikiTarix.diff(kecmisTarix, 'months'); // 24 ay
+
+// console.log(illerArasindakiFerq);
+// console.log(c);
+
+// let myObj = {    
+//     fullName:"John",
+//     islediyiZaman:illerArasindakiFerq,
+//     umumiAldigimaaas:illerArasindakiFerq * 2000, 
+//     maas:2000
+// }
+
+// let time = moment().set('year', 2022) ;
+
+// console.log(time);
+// console.log(moment().diff(moment(randomdate, "DD-MM-YYYY"), "years"));
+
+//Now
+// console.log(moment(randomdate,randomFormat).fromNow());
+// console.log(moment(randomdate,randomFormat).toNow());
+
+
+
