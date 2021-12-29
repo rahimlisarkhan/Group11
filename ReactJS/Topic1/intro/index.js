1e399f97e624f1ddeaf3e5ba69316de5925f9bc7
rahimlisarkhan <rahimlisarkhan@gmail.com>
2021-12-29 13:42:35 +0400

    update topic1

---
+++ ReactJS/Topic1/intro/index.js
@@ -0,0 +1,13 @@
+
+
+class App extends React.Component {
+
+    render() {
+        return (
+            <h1>Salam</h1>
+        )
+    }
+}
+
+
+ReactDOM.render(<App />, document.getElementById('root'))
